0fdbc777eb37af3f9b7ba85b783856ebd2d32642
Imane <ibarakate0@gmail.com>
2025-01-14 12:14:21 +0100

    added background

--- 7-Boids/sketch.js
+++ 7-Boids/sketch.js
@@ -142,29 +142,16 @@ function createSharks(num) {
 }
 
 function draw() {
-  background(239, 276, 250);
-
-  fill(118, 170, 206);
-  // We are going to draw a polygon out of the wave points
+  background(0, 0, 0);
+  fill(118, 170, 286);
   beginShape();
 
-  let xPos = 0; // Option #1: 2D Noise
-  // let xoff = yoff; // Option #2: 1D Noise
-
-  // Iterate over horizontal pixels
+  let xPos = 0; 
   for (let x = 0; x <= width; x += 10) {
-    // Calculate a y value according to noise, map to
-
-    // Option #1: 2D Noise
     let y = map(noise(xPos, yPos), 0, 1, 200, 300);
-
-
-    // Set the vertex
     vertex(x, y);
-    // Increment x dimension for noise
     xPos += 0.05;
   }
-  // increment y dimension for noise
   yPos += 0.01;
   vertex(width, height);
   vertex(0, height);
